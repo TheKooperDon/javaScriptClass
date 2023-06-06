e6ad2fe258c51406ae3aee1e2d7be0265b6edab8
TheKooperDon <valodinejr@gmail.com>
2023-06-06 09:29:32 -0500

    did ex1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,32 @@
 let js = 'amazing';
-if (js === 'amazing') alert('js is FUN!');
+//if (js === 'amazing') alert('js is FUN!');
 console.log(40 + 2 + 2 + 2);
+
+console.log('Jonas');
+
+let firstName = 'Bob';
+
+console.log(firstName);
+
+let myFirstJob = 'Programmer'
+
+let myCurrentJob = 'Teacher'
+
+//vs doing it this way
+
+let job1 = 'progammer'
+let job2 = 'teacher'
+
+//that way is ugly^
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console*/
+
+
+let country = 'USA';
+let continent = 'North America';
+let population = '332000000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
