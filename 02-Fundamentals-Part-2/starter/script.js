3d0d3424dbdfbf4d3c35c987b1031837a2e8eab5
TheKooperDon <valodinejr@gmail.com>
2023-06-12 11:43:46 -0500

    challenge done even tho its glitched.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,47 +1,6 @@
-/*
-
-let scoreDolphins = ((96 + 108 + 89) / 3);
-let scoreKoalas = ((88 + 91 + 110) / 3);
-
-console.log(scoreKoalas, scoreDolphins);
-
-if (scoreDolphins > scoreKoalas) {
-    console.log(`Dolphins win the trophy`)
-} else if (scoreDolphins === scoreKoalas) {
-    console.log(`Both win the trophy`)
-} else {
-    console.log(`Koalas win the trophy`)
-}
-
-scoreDolphins = ((97 + 112 + 101) / 3);
-scoreKoalas = ((109 + 95 + 123) / 3);
-console.log(scoreDolphins)
-console.log(scoreKoalas)
-
-if ((scoreDolphins >= 100) && scoreDolphins > scoreKoalas) {
-    console.log(`Dolphins win the trophy and is over a 100`)
-} else if (scoreDolphins === scoreKoalas) {
-    console.log(`Both win the trophy`)
-} else if ((scoreKoalas >= 100) && (scoreKoalas > scoreDolphins)) {
-    console.log(`Koalas win and its over a 100`)
-} else {
-    console.log(`no one scored over a 100`)
-}
-
-scoreKoalas = ((109 + 95 + 106) / 3);
 
 
-console.log(scoreDolphins);
-console.log(scoreKoalas);
-if (scoreDolphins > scoreKoalas && (scoreDolphins || scoreKoalas >= 100)) {
-    console.log(`Dolphins win the trophy`)
-} else if ((scoreDolphins > 100) && (scoreKoalas > 100) && (scoreDolphins === scoreKoalas)) {
-    console.log(`Both win the trophy and its over 100`)
-} else {
-    console.log(`Koalas win the trophy`)
-}
 
-*/
 
 
 
