5661175ffd8f6eb716e1e090e36da0a54431a8ce
TheKooperDon <valodinejr@gmail.com>
2023-08-09 21:01:57 -0600

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -7,9 +7,31 @@
 
 //challenge 3
 
+const mark = {
+    fullName: `Mark Miller`,
+    mass: 78,
+    height: 1.69,
 
-const maker = (fullName, mass, height) => {
+    calcBMI: function () {
+        return `${this.mass / (this.height * this.height)}`
+    }
+};
 
-
+const john = {
+    fullName: `John Smith`,
+    mass: 92,
+    height: 1.95,
+    //this needs to be a property of bmi 
+    calcBMI: function () {
+        return `${this.mass / (this.height * this.height)}`
+    }
 }
 
+console.log
+
+//const maker = (fullName, mass, height) => {
+
+
+//}
+
+console.log(`${john.fullName}'s BMI (${john.calcBMI()} is higher than ${mark.fullName} (${mark.calcBMI()})!)`)
