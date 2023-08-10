5772253e4578ed926d1f182f537723bd6609525a
TheKooperDon <valodinejr@gmail.com>
2023-08-10 12:13:22 -0600

    challenge 3 done

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,35 +5,3 @@
 'use strict';
 
 
-//challenge 3
-
-const mark = {
-    fullName: `Mark Miller`,
-    mass: 78,
-    height: 1.69,
-
-    calcBMI: function () {
-        `${this.mass / (this.height * this.height)}`;
-        return
-    }
-};
-
-const john = {
-    fullName: `John Smith`,
-    mass: 92,
-    height: 1.95,
-    //this needs to be a property of bmi 
-    calcBMI: function () {
-        return `${this.mass / (this.height * this.height)}`
-    }
-}
-
-console.log
-
-//const maker = (fullName, mass, height) => {
-
-
-//}
-console.log(john.calcBMI());
-console.log(mark.calcBMI());
-console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName} (${mark.calcBMI()})!)`)
--- challengesPart2/challenge7.js
+++ challengesPart2/challenge7.js
@@ -17,3 +17,48 @@ TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 
 
 IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
 */
+
+//challenge 3
+
+const mark = {
+    fullName: `Mark Miller`,
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+/*
+ calcBMI: function () {
+        return this.mass / (this.height * this.height);
+ }
+
+};
+mark.bmi = mark.calcBMI();
+
+So i guess this was wrong and we could make the whole fuction return that bmi thing inside the function. 
+
+*/
+
+
+const john = {
+    fullName: `John Smith`,
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+
+};
+
+
+//const maker = (fullName, mass, height) => {
+
+
+//}
+
+console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})!)`)
