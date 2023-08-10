f80d63f3a6a85d6c642bf9770431a27fa8585643
TheKooperDon <valodinejr@gmail.com>
2023-08-10 12:21:33 -0600

    Update challenge7.js

--- challengesPart2/challenge7.js
+++ challengesPart2/challenge7.js
@@ -62,3 +62,15 @@ const john = {
 //}
 
 console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})!)`)
+
+
+//or 
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})!)`)
+
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})!)`)
+} else {
+    console.log(`They have the same BMI`)
+}
