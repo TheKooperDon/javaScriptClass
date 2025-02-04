0b08ba54eff25e5ed802f5522f4fb355c9c77274
TheKooperDon <valodinejr@gmail.com>
2025-02-04 11:55:30 -0700

    starting back

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+//getElementById also works here instead of query
+//document.querySelector is pulling from the html
+const score1El = document.querySelector('#score--1');
+
+// this is making that number start as 0 if you made it 52 it would start at 52
+score0El.textContent = 0;
+score1El.textContent = 0;
