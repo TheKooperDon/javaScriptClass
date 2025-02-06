1722e9e9275a4d97206f43494d02b064e01b5d9b
TheKooperDon <valodinejr@gmail.com>
2025-02-06 11:54:09 -0700

    button attempt
    i am struggling to get a button to work here

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+const button = document.querySelectorAll('.cute-button');
+const jeff = document.querySelectorAll('.jeff');
+const jeffFont = document.getElementById('jeffFont');
+
+button.addEventListener('click', function () {
+  jeffFont.style.fontfamily = 'Arial, sans-serif';
+});
