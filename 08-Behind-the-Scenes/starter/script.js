8c3c2760a63bf5c93988116c72614e081e93ce75
TheKooperDon <valodinejr@gmail.com>
2025-02-07 13:38:13 -0700

    button font change woot but needs more love
    do while loops is soon

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -3,13 +3,41 @@ const button = document.querySelectorAll('.cute-button');
 const leftButton = button[0];
 const middleButton = button[1];
 const rightButton = button[2];
-const jeffHeading = document.querySelector('jeffSyle');
+const jeffHeading = document.querySelector('.jeffStyle'); // Correct spelling
+const randomFonts = [
+  "Brush Script MT', cursive",
+  'Arial, sans-serif',
+  'Courier New, monospace',
+  'Georgia, serif',
+  'Times New Roman, Times, serif',
+  'Verdana, sans-serif',
+];
+const randomIndex = Math.floor(Math.random() * randomFonts.length);
+const randomFont = randomFonts[randomIndex];
 
 /*leftButton.addEventListener('click', function () {
   jeffFont.style.fontFamily = 'Arial, sans-serif';
 });
 
 */
+//fonts
 leftButton.addEventListener('click', function () {
   console.log('Left button clicked!');
+  //jeffHeading.style.fontFamily = 'arial';
+  jeffHeading.style.fontFamily = randomFont;
+  // to keep changing i need to do a do whole loop we are gonna pause this idea
+
+  //do the other buttons now see if we can do it just on our own.
+  //randomFonts.style.Math.floor(Math.random() * randomFonts.length);
+  console.log('did i even choose a font', randomFont);
+  console.log('jeffHeading');
+});
+//size
+middleButton.addEventListener('click', function () {
+  console.log('middle button clicked!');
+});
+
+//color
+rightButton.addEventListener('click', function () {
+  console.log('right button clicked!');
 });
