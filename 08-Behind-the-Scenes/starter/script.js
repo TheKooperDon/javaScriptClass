4f36933b2de0f3924b150af1e9519d3b2b5620c3
TheKooperDon <valodinejr@gmail.com>
2025-02-13 11:55:45 -0700

    my god this lesson is so boring

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+  console.log(firstName);
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (birthYear > 1981 && birthYear <= 1996) {
+      const str = `Oh you a millenial, ${firstName}`;
+      console.log(str);
+      /*function add(a, b){
+        return a + b;
+      }*/
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jeff';
+calcAge(1996);
