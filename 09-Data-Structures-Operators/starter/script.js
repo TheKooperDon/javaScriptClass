a006b92a5753803cf13277a12ec3c003eb20564c
TheKooperDon <valodinejr@gmail.com>
2025-03-05 20:11:29 -0700

    getting better

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,21 +11,12 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex ]];
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
+
+  
 };
 
 
@@ -37,9 +28,34 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
-const [main, , secondary] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
-
+/*
 const temp = main;
 main = secondary;
 secondary = temp;
+console.log(main, secondary);
+
+*/
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+console.log(restaurant.order(2, 0));
+// returning 2 vars in a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+
+const nested = [2,4,[5,6]];
+//const [i, , j] = nested;
+//console.log(i, j);
+
+const [i, , [j, k]] = nested;
+
+console.log(i, j, k); 
+
+//default values
+
+const [p, q, r] = [8, 9];
+console.log(p, q, r)
