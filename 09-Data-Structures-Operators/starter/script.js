8d911d64b21450d5915743e289308291378899f3
TheKooperDon <valodinejr@gmail.com>
2025-03-06 09:46:42 -0700

    starting challenges part 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -19,7 +19,7 @@ const restaurant = {
   
 };
 
-
+/*
 const arr = [2,3,4];
 const a = arr[0];
 const b = arr[1];
@@ -28,6 +28,7 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
+*/
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 /*
